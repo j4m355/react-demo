700506b68382caedaa38ad971e6da0886938e43d
James Campbell <e@mailjames.com>
2016-05-25 22:28:10 +0100

    router.historylocation

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,8 @@ var React = require('react');
 var Router = require('react-router');
 var DefaultRoute = Router.DefaultRoute;
 var Route = Router.Route;
+var NotFoundRoute = Router.NotFoundRoute;
+var Redirect = Router.Redirect;
 
 
 var routes = (
@@ -11,6 +13,8 @@ var routes = (
 		<DefaultRoute handler={require('./components/homePage')} />
 		<Route name="authors" handler={require('./components/authors/authorPage')} />
 		<Route name="about" handler={require('./components/about/aboutPage')} />
+		<NotFoundRoute handler={require('./components/notFoundPage')} />
+		<Redirect from="about-us" to="about" />
 	</Route>
 );
 
