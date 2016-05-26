ff694598c9b6db96d58cd4929c89383e1e6968e7
James Campbell <e@mailjames.com>
2016-05-26 01:35:28 +0100

    reuseable-input

---
+++ src/components/authors/authorForm.js
@@ -0,0 +1,36 @@
+"use strict";
+
+var React = require('react');
+var Input = require('../common/textInput');
+
+var AuthorForm = React.createClass({
+
+	render: function() {
+		return (
+			<form>
+				<h1>Manage Author</h1>
+				<Input 
+					name="firstName"
+					label="First Name"
+					value={this.props.author.firstName}
+					placeholder="First Name"
+					onChange={this.props.onChange}
+					/>
+
+
+				<Input 
+					name="lastName"
+					label="Last Name"
+					value={this.props.author.lastName}
+					placeholder="Last Name"
+					onChange={this.props.onChange}
+					/>
+
+
+				<input type="submit" value="Save" className="btn btn-default" />
+			</form>
+		);
+	}
+});
+
+module.exports = AuthorForm;
